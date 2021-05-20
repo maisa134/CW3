e44b4dd1eadcbfca85575a4851cc4247e2b2dada
maisa134 <73913853+maisa134@users.noreply.github.com>
2021-05-20 21:42:53 +0100

    added product list

---
+++ app/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+    <ListView for='product in products' @itemTap='onItemTap'> 
+    <v-template>
+        <StackLayout>
+        <Label :text='product.topic'/>
+        <Label :text="`price: ${product.cost}`"/>
+        </StackLayout>
+    </v-template>
+    </ListView>
+</template>
+
+<script>
+export default {
+    data() {
+       return {
+            products: [
+                { topic: "English", cost: 5, spaces: 10, location: "NW1" }
+            ],
+       };
+    },
+    methods: {
+        onItemTap(event) {
+            alert("Added to cart: " + event.item.name);
+            this.$emit('addProduct', event.item);
+        }
+    }
+}
+</script>
